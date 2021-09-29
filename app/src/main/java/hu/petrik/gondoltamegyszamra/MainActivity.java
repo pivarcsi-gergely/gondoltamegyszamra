b7913e7c741dd3bd0a53960c882312c7553dff1e
Pivarcsi Gergely Mate <gergely.pivarcsi@gmail.com>
2021-09-29 19:01:42 +0200

    2nd commit (nagyon egyveleg)

--- app/src/main/java/hu/petrik/gondoltamegyszamra/MainActivity.java
+++ app/src/main/java/hu/petrik/gondoltamegyszamra/MainActivity.java
@@ -3,12 +3,58 @@ package hu.petrik.gondoltamegyszamra;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+private ImageView hp1, hp2, hp3, hp4;
+private Button buttonMinusz, buttonPlusz, buttonTipp;
+private TextView textViewSzam;
+private int tippeltSzam;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        buttonMinusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tippeltSzam == 0){
+                    // TODO: Felugró ablak fog jönni
+                }
+                else{
+                    tippeltSzam--;
+                    textViewSzam.setText(String.valueOf(tippeltSzam));
+                }
+            }
+        });
+        buttonPlusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (tippeltSzam >= 10){
+                    // TODO: Felugró ablak fog jönni
+                }
+                else{
+                    tippeltSzam++;
+                    textViewSzam.setText(String.valueOf(tippeltSzam));
+                }
+            }
+        });
+    }
+
+    public void init(){
+        tippeltSzam = 1;
+        hp1 = findViewById(R.id.imageViewHp1);
+        hp2 = findViewById(R.id.imageViewHp2);
+        hp3 = findViewById(R.id.imageViewHp3);
+        hp4 = findViewById(R.id.imageViewHp4);
+        buttonMinusz = findViewById(R.id.buttonMinusz);
+        buttonPlusz = findViewById(R.id.buttonPlusz);
+        buttonTipp = findViewById(R.id.buttonTipp);
+        textViewSzam = findViewById(R.id.textViewSzam);
     }
 }
