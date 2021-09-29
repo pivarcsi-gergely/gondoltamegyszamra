5b5ea150e224ede6f70355b6ad44d9e775c4d765
Pivarcsi Gergely Mate <gergely.pivarcsi@gmail.com>
2021-09-29 19:03:50 +0200

    random változók

--- app/src/main/java/hu/petrik/gondoltamegyszamra/MainActivity.java
+++ app/src/main/java/hu/petrik/gondoltamegyszamra/MainActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 private ImageView hp1, hp2, hp3, hp4;
 private Button buttonMinusz, buttonPlusz, buttonTipp;
 private TextView textViewSzam;
 private int tippeltSzam;
+private Random random;
+private int gondoltSzam;
 
 
     @Override
@@ -48,6 +53,8 @@ private int tippeltSzam;
 
     public void init(){
         tippeltSzam = 1;
+        random = new Random();
+        gondoltSzam = random.nextInt(10)+1;
         hp1 = findViewById(R.id.imageViewHp1);
         hp2 = findViewById(R.id.imageViewHp2);
         hp3 = findViewById(R.id.imageViewHp3);
