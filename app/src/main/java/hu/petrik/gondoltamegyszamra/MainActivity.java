82fcac53f7642e81d87fc3a13a11e1948c59703a
Mate501 <jurancsik.mate501@gmail.com>
2021-10-06 17:46:56 +0200

    Toast-ok kész + AlertDialog-ok

--- app/src/main/java/hu/petrik/gondoltamegyszamra/MainActivity.java
+++ app/src/main/java/hu/petrik/gondoltamegyszamra/MainActivity.java
@@ -1,14 +1,19 @@
 package hu.petrik.gondoltamegyszamra;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -20,6 +25,8 @@ private int tippeltSzam;
 private Random random;
 private int gondoltSzam;
 private int elet;
+private Toast customToast;
+private AlertDialog.Builder alerBuilder;
 
 
     @Override
@@ -58,13 +65,17 @@ private int elet;
                     //TODO: Felugró ablak fog jönni, "Nyertél!" felirattal
                 }
                 else if(tippeltSzam < gondoltSzam){
-                    //TODO: Felugró ablak (nagyobb szám)
+                    //Felugró ablak (nagyobb szám)
                     elet--;
                     eletMinusz();
+                    customToast.show();
+                    Toast.makeText(getApplicationContext(), "Nagyobb számra gondoltam", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    //TODO: Felugró ablak jön (kisebb szám)
-
+                    //Felugró ablak jön (kisebb szám)
+                    eletMinusz();
+                    customToast.show();
+                    Toast.makeText(getApplicationContext(), "Kisebb számra gondoltam", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -102,5 +113,33 @@ private int elet;
         buttonPlusz = findViewById(R.id.buttonPlusz);
         buttonTipp = findViewById(R.id.buttonTipp);
         textViewSzam = findViewById(R.id.textViewSzam);
+        customToast = new Toast(getApplicationContext());
+        CreateCustomToast();
+        alerBuilder = new AlertDialog.Builder(getApplicationContext());
+        CreateAlertDialog();
+    }
+
+    private void CreateAlertDialog() {
+        alerBuilder.setMessage("Szeretnél-e új játékot?");
+        alerBuilder.setNegativeButton("Nem", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        alerBuilder.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ujJatek();
+                closeContextMenu();
+            }
+        })
+    }
+
+    private void CreateCustomToast() {
+        customToast.setDuration(Toast.LENGTH_SHORT);
+        View view = getLayoutInflater().inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.customToast));
+        customToast.setView(view);
+        customToast.setGravity(Gravity.CENTER, 0, 0);
     }
 }
